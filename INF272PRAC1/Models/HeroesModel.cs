8ef5927c75692e84237145b7be604133c7cfd27b
pitsosekhoto <pitsosekhoto@PITSOSEKHOT69AF>
2024-03-11 18:59:06 +0200

    Add project files.

---
+++ INF272PRAC1/Models/HeroesModel.cs
@@ -0,0 +1,13 @@
+﻿namespace INF272PRAC1.Models
+{
+    public class HeroesModel
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public int? Age { get; set; }
+        public string? Birthday { get; set; }
+        public required string Height { get; set; }
+        public required string Image { get; set; }
+        public bool Alive { get; set; }
+    }
+}
